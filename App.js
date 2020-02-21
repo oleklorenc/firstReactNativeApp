bdd17f692ca39750347d430b8630c0b898f0461c
Aleksander Lorenc <oleklorenc2000@gmail.com>
2020-02-21 12:00:30 +0100

    increment and decrement app

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+  function SetCounterHandler(counter, x) {
+    setCounter(counter + x);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <Text style={styles.text}>HELLO REACT NATIVE</Text>
+      <Text style={styles.counter}>{counter}</Text>
+      <View style={styles.buttons}>
+        <Button
+          title="increment"
+          onPress={() => {
+            SetCounterHandler(counter, 1);
+          }}
+        />
+        <Button
+          title="decrement"
+          onPress={() => {
+            SetCounterHandler(counter, -1);
+          }}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center"
   },
+  text: {
+    fontSize: 30,
+    marginTop: 30
+  },
+  counter: {
+    fontSize: 20,
+    margin: 100
+  },
+  buttons:{
+    flexDirection: "row",
+  }
 });
